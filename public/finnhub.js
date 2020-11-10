0dcdfe181f95397e2da0c8b0631596d34631e9a2
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2020-11-10 15:43:57 -0500

    add finnhub company news and stock quote code

--- public/finnhub.js
+++ public/finnhub.js
@@ -1,23 +1,31 @@
 // finnhub
 
 /*
-const finnhub = require('finnhub');
- 
+var finnhub = require('finnhub')
+var request = require('request')
+var token = 'buko60n48v6qi7364sag'
+
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = "buko60n48v6qi7364sag" 
+api_key.apiKey = token // Replace this
 const finnhubClient = new finnhub.DefaultApi()
 
 //Company News
-function getCompanyNews() {
-    finnhubClient.companyNews("AAPL", "2020-01-01", "2020-05-01", (error, data, response) => {
-        if (error) {
-            console.error(error);
-        } 
-        else {
-            console.log(data)
-        }
+// finnhubClient.companyNews("AAPL", "2020-11-08", "2020-11-09", (error, data, response) => {
+//     if (error) {
+//         console.error(error);
+//     } else {
+//         console.log('received company news')
+//         console.log(data)
+//     }
+// });
+
+// stock quote
+request(`https://finnhub.io/api/v1/quote?symbol=AAPL&token=${token}`, { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log('successfully received stock information')
+  console.log(body);
 });
-}
+
 */
 const express = require('express');
 const app     = express();
