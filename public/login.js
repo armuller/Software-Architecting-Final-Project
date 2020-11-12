ae6fecb6d3f109236a75a81adec032907f9c645f
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2020-11-11 23:24:48 -0500

    Add messaging for log in/ log out/ sign up

--- public/login.js
+++ public/login.js
@@ -11,7 +11,8 @@
         messagingSenderId: "321737945642",
         appId: "1:321737945642:web:2c3800f0ff3bb40841c953",
         measurementId: "G-Q52M7VL8WN"
-    };
+	};
+	const target = document.getElementById('home_target');
 
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
@@ -28,7 +29,13 @@
 	login.addEventListener('click', e => {
 		const auth  = firebase.auth();		
 		const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-		promise.catch(e => console.log(e.message));
+		promise.catch(e => {
+			console.log(e.message)
+			target.innerHTML = `
+				<div class="alert alert-warning mt-3" role="alert">
+				Failed to log in. Account does not exist or email and password is incorrect. Please sign up for an account or double check your email and password is correct
+			</div>`
+		});
 	});
 
 	// signup
@@ -36,13 +43,24 @@
 		// TODO: check for real email
 		const auth  = firebase.auth();
 		const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
-		promise.catch(e => console.log(e.message));
+		promise.catch(e => {
+			console.log(e.message)
+			target.innerHTML = `
+				<div class="alert alert-danger mt-3" role="alert">
+				There was an error creating the account: ${e.message}. Please try again
+			</div>`
+		});
 	});
 
     // logout
 	logout.addEventListener('click', e => {
 		firebase.auth().signOut();
+		target.innerHTML = `
+			<div class="alert alert-success mt-3" role="alert">
+			Successfully logged out!
+		  </div>`
 	});
+
 
     // login state
 	firebase.auth().onAuthStateChanged(firebaseUser => {
@@ -51,6 +69,10 @@
 			logout.style.display = 'inline';
 			login.style.display  = 'none';
 			signup.style.display = 'none';
+			target.innerHTML = `
+			<div class="alert alert-success mt-3" role="alert">
+			Successfully logged in! Welcome, ${firebaseUser.email}
+		  </div>`
 		}
 		else{
 			console.log('User is not logged in');
