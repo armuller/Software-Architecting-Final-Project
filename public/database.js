23312cb069b1612a0316f81833aad5854c8e1771
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2020-11-17 01:47:25 -0500

    get async getCurrentUser to work, get current price data from finnhub when buy/ selling stocks

--- public/database.js
+++ public/database.js
@@ -1,107 +1,133 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyCfJP3rWp_QZw-cnTdunHbhiG_Ms6hrml8",
-    authDomain: "final-project-f6408.firebaseapp.com",
-    databaseURL: "https://final-project-f6408.firebaseio.com/",
-    projectId: "final-project-f6408",
-    storageBucket: "final-project-f6408.appspot.com",
-    };
+  apiKey: "AIzaSyCfJP3rWp_QZw-cnTdunHbhiG_Ms6hrml8",
+  authDomain: "final-project-f6408.firebaseapp.com",
+  databaseURL: "https://final-project-f6408.firebaseio.com/",
+  projectId: "final-project-f6408",
+  storageBucket: "final-project-f6408.appspot.com",
+};
 // Initialize Firebase
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
 // handle on firebase db
 var db = firebase.database();
 
 function addNewUser() {
-    const status  = document.getElementById('status');
+  const status = document.getElementById("status");
 
-    const firebaseUser = firebase.auth().currentUser
-    console.log('current user is: ' + firebaseUser)
-    const email = firebaseUser.email
-    const name = email.substring(0, email.indexOf('@')) // todo: can probably update to ask for the user's name, but for now just take whatever is in front of '@'
-    const users = db.ref('users')
-    const userId = firebaseUser.uid
-    const balance = 0;
+  const firebaseUser = firebase.auth().currentUser;
+  console.log("current user is: " + firebaseUser);
+  const email = firebaseUser.email;
+  const name = email.substring(0, email.indexOf("@")); // todo: can probably update to ask for the user's name, but for now just take whatever is in front of '@'
+  const users = db.ref("users");
+  const userId = firebaseUser.uid;
+  const balance = 0;
 
-    // write to db
-    users.child(userId).set({email, name, balance})
-    .then(function(){
-        console.log(`successfully added new user to DB: ${email}`)
-        status.innerHTML = "Wrote to DB!";
+  // write to db
+  users
+    .child(userId)
+    .set({ email, name, balance })
+    .then(function () {
+      console.log(`successfully added new user to DB: ${email}`);
+      status.innerHTML = "Wrote to DB!";
     })
-    .catch(function(err) {
-        console.log(`failed added new user to DB: ${email}`)
-        status.innerHTML = 'There was an error. did not write to DB';
+    .catch(function (err) {
+      console.log(`failed added new user to DB: ${email}`);
+      status.innerHTML = "There was an error. did not write to DB";
     });
 }
 
 function getAllUsers() {
-    const status  = document.getElementById('status');
-    return db.ref('/users/').once('value').then(function(snapshot) {
-        console.log(snapshot.val())
-        // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-        // ...
-        status.innerHTML = "Got all users from DB"
-      });
+  const status = document.getElementById("status");
+  return db
+    .ref("/users/")
+    .once("value")
+    .then(function (snapshot) {
+      console.log(snapshot.val());
+      // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+      // ...
+      status.innerHTML = "Got all users from DB";
+    });
 }
 
 function getCurrentUser() {
-    const userId = firebase.auth().currentUser.uid
-    let results = null
-    const status  = document.getElementById('status');
-    return db.ref('/users/' + userId).once('value').then(function(snapshot) {
-        console.log(snapshot.val())
-        results = snapshot.val()
+  const userId = firebase.auth().currentUser.uid;
+  let results = null;
+  const status = document.getElementById("status");
+  return new Promise((resolve, reject) => {
+    db.ref("/users/" + userId)
+      .once("value")
+      .then(function (snapshot) {
+        console.log(snapshot.val());
+        results = snapshot.val();
         // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
         // ...
-        status.innerHTML = "Got current user from DB: " + JSON.stringify(results)
+        status.innerHTML =
+          "Got current user from DB: " + JSON.stringify(results);
+        return results;
+      })
+      .then((result) => {
+        resolve(result);
       });
+  });
 }
 
+// todo: check if number is negative that the user has enough shares to sell. If the number is positive user has enough money to buy
+// todo: transactions needs to be a list we continuously append to, not an object that keeps replacing itself
 function purchaseStock() {
-    const userId = firebase.auth().currentUser.uid
-    // console.log(getCurrentUser())
-    // note: for some reason because getCurrentUser() is an unresolved promise I was having
-    // undefined issues...so I decided to just do it here instaed of doing promise chaining
-    const status  = document.getElementById('status');
-    db.ref('/users/' + userId).once('value').then(function(snapshot) {
-        currentUser = snapshot.val()
-        console.log('current user information is')
-        console.log(currentUser)
-        currentUser['transactions'] = {
-            'symbol': 'AAPL',
-            'purchase_date': new Date().getTime(),
-            'number of shares': 2
-        }
-        console.log('current user after stock purchase')
-        console.log(currentUser)
-        var updates = {}
-        updates[userId] = currentUser
-        status.innerHTML = "Purchased stock!"
-        return db.ref('users').update(updates)
-        
-      });     
+  const userId = firebase.auth().currentUser.uid;
+  const status = document.getElementById("status");
+  const stock = document.getElementById("stock_ticker").value;
+  const numShares = parseInt(document.getElementById("num_shares").value);
+  (async () => {
+    const stockQuoteInfo = await getStockQuote(stock);
+    // const currentPrice = getStockQuote(stock)
+    console.log("stock quote info is:");
+    console.log(stockQuoteInfo);
+    const currentPrice = stockQuoteInfo.c;
+    const currentUser = await getCurrentUser();
+    // console.log('user is')
+    // console.log(currentUser)
+    currentUser["transactions"] = {
+      symbol: stock,
+      purchase_date: new Date().getTime(),
+      "number of shares": numShares,
+      price: currentPrice, // get this info from finnhub
+    };
+    console.log("current user after stock purchase");
+    console.log(currentUser);
+    var updates = {};
+    updates[userId] = currentUser;
+    status.innerHTML =
+      numShares > 0
+        ? `Purchased ${numShares} shares of ${stock}!`
+        : `Sold ${numShares} shares of ${stock}!`;
+    return db.ref("users").update(updates);
+  })();
 }
 
 function depositFunds() {
-    const depositAmount = parseFloat(document.getElementById('deposit_amount').value)
-    console.log('deposit amount is ' + depositAmount)
-    const userId = firebase.auth().currentUser.uid
-    const status  = document.getElementById('status');
-    db.ref('/users/' + userId).once('value').then(function(snapshot) {
-        currentUser = snapshot.val()
-        console.log('current user information is')
-        console.log(currentUser)
-        const balance = parseFloat(currentUser['balance']) + depositAmount
-        currentUser['balance'] = parseFloat(balance);
-        console.log('current user after deposit')
-        console.log(currentUser)
-        var updates = {}
-        updates[userId] = currentUser
-        status.innerHTML = `Deposited $${depositAmount}! New balance is: $${balance}`
-        return db.ref('users').update(updates)
-  });
+  const depositAmount = parseFloat(
+    document.getElementById("deposit_amount").value
+  );
+  console.log("deposit amount is " + depositAmount);
+  const userId = firebase.auth().currentUser.uid;
+  const status = document.getElementById("status");
+  (async () => {
+    const currentUser = await getCurrentUser();
+    // console.log('user is')
+    console.log("current user information is");
+      console.log(currentUser);
+      const balance = parseFloat(currentUser["balance"]) + depositAmount;
+      currentUser["balance"] = parseFloat(balance);
+      console.log("current user after deposit");
+      console.log(currentUser);
+      var updates = {};
+      updates[userId] = currentUser;
+      status.innerHTML = `Deposited $${depositAmount}! New balance is: $${balance}`;
+      return db.ref("users").update(updates);
+  })();
 }
--- public/finnhub.js
+++ public/finnhub.js
@@ -1,119 +1,132 @@
 function getCompanyNews() {
-    var ticker = document.getElementById('company').value;
-    var endDate = parseInt(new Date().getTime() / 1000)
-    var startDate = endDate - 30 * 24 * 60 * 60;
+  var ticker = document.getElementById("company").value;
+  var endDate = parseInt(new Date().getTime() / 1000);
+  var startDate = endDate - 30 * 24 * 60 * 60;
 
-    var url = '/companynews/' + ticker + '/' + startDate + '/' + endDate;
+  var url = "/companynews/" + ticker + "/" + startDate + "/" + endDate;
 
+  superagent.get(url).end(function (err, res) {
+    if (err) {
+      console.log(err);
+      document.getElementById("target").innerHTML =
+        "Acquiring Company News Failed!";
+    } else {
+      console.log("Company News Acquired!");
+      document.getElementById("target").innerHTML = JSON.stringify(res.body);
+    }
+  });
+}
+
+function getStockQuote(quote = null) {
+  var ticker = document.getElementById("company").value;
+  if (quote != null) {
+    ticker = quote;
+  }
+
+  var url = "/stockquote/" + ticker;
+
+  return new Promise((resolve, reject) => {
+    console.log("making super agent call");
     superagent
-        .get(url)
-        .end(function (err, res) {
-            if (err) {
-                console.log(err);
-                document.getElementById("target").innerHTML = "Acquiring Company News Failed!";
-            }
-            else {
-                console.log('Company News Acquired!');
-                document.getElementById("target").innerHTML = JSON.stringify(res.body);
-            }
-        });
-};
-
-function getStockQuote() {
-    var ticker = document.getElementById('company').value;
-
-    var url = '/stockquote/' + ticker;
-
-    superagent
-        .get(url)
-        .end(function (err, res) {
-            if (err) {
-                console.log(err);
-                document.getElementById("target").innerHTML = "Acquiring Company Quote Failed!";
-            }
-            else {
-                console.log('Company Quote Acquired!');
-                //document.getElementById("stock_quote").innerHTML = JSON.stringify(res.body);
-                document.getElementById("ticker").innerHTML = ticker;
-                document.getElementById("open").innerHTML = "$"+round(res.body.o,2);
-                document.getElementById("current").innerHTML = "$"+round(res.body.c,2);
-            }
-        });
-};
-
+      .get(url)
+      .then(function (res) {
+        console.log("Company Quote Acquired!");
+        console.log(res.body);
+        //document.getElementById("stock_quote").innerHTML = JSON.stringify(res.body);
+        document.getElementById("ticker").innerHTML = ticker;
+        document.getElementById("open").innerHTML = "$" + round(res.body.o, 2);
+        document.getElementById("current").innerHTML =
+          "$" + round(res.body.c, 2);
+        return res.body;
+      })
+      .then((result) => {
+        resolve(result);
+      });
+  });
+}
 
 function getCandleStick() {
-    var ticker = document.getElementById('company').value;
-    var endDate = parseInt(new Date().getTime() / 1000)
-    var startDate = endDate - 30 * 24 * 60 * 60;
+  var ticker = document.getElementById("company").value;
+  var endDate = parseInt(new Date().getTime() / 1000);
+  var startDate = endDate - 30 * 24 * 60 * 60;
 
-    var url = '/candlestick/' + ticker + '/' + startDate + '/' + endDate;
+  var url = "/candlestick/" + ticker + "/" + startDate + "/" + endDate;
 
-    superagent
-        .get(url)
-        .end(function (err, res) {
-            if (err) {
-                console.log(err);
-                document.getElementById("stock_graph_canvas").innerHTML = "Acquiring CandleStick Failed!";
-            }
-            else {
-                console.log('Company Candle Stick Acquired!');
-                
-                var i;
-                var time = [];
-                for (i=0; i < res.body.t.length; i++) {
-                    time.push(timeConverter(res.body.t[i]));
-                }
-                console.log(time);
-                var myLineChart2 = new Chart(
-                  document.getElementById("stock_graph_canvas"),
-                  {
-                    type: "line",
-                    data: {
-                      labels: time,
-                      datasets: [
-                        {
-                          label: ticker + " Stock Price",
-                          data: res.body.c,
-                          fill: false,
-                          borderColor: "rgb(75, 192, 192)",
-                          lineTension: 0.1,
-                        },
-                      ],
-                    },
-                    options: {},
-                  }
-                );
-            }
-        });
-};
+  superagent.get(url).end(function (err, res) {
+    if (err) {
+      console.log(err);
+      document.getElementById("stock_graph_canvas").innerHTML =
+        "Acquiring CandleStick Failed!";
+    } else {
+      console.log("Company Candle Stick Acquired!");
 
+      var i;
+      var time = [];
+      for (i = 0; i < res.body.t.length; i++) {
+        time.push(timeConverter(res.body.t[i]));
+      }
+      console.log(time);
+      var myLineChart2 = new Chart(
+        document.getElementById("stock_graph_canvas"),
+        {
+          type: "line",
+          data: {
+            labels: time,
+            datasets: [
+              {
+                label: ticker + " Stock Price",
+                data: res.body.c,
+                fill: false,
+                borderColor: "rgb(75, 192, 192)",
+                lineTension: 0.1,
+              },
+            ],
+          },
+          options: {},
+        }
+      );
+    }
+  });
+}
 
 function buyAndSell() {
-    getStockQuote();
-    getCandleStick();
+  getStockQuote();
+  getCandleStick();
 }
 
 // Got this from stack overflow
-function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-    return time;
-  }
-  console.log(timeConverter(0));
+function timeConverter(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time =
+    date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
+  return time;
+}
+console.log(timeConverter(0));
 
 // Got this from stack overflow
 function round(value, decimals) {
-    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-  }
-
+  return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
+}
 
 // stock quote
 // request(`https://finnhub.io/api/v1/quote?symbol=AAPL&token=${token}`, { json: true }, (err, res, body) => {
