353a720f9b359391916dfadddf857ba981573e39
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2020-11-25 02:57:37 -0500

    log in/ log out don't work properly yet...

--- public/login.js
+++ public/login.js
@@ -13,59 +13,101 @@
         appId: "1:321737945642:web:2c3800f0ff3bb40841c953",
         measurementId: "G-Q52M7VL8WN"
 	};
-	const target = document.getElementById('home_target');
+	// const target = document.getElementById('home_target');
 
     // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
+    if (!firebase.apps.length) {
+		firebase.initializeApp(firebaseConfig);
+		firebase.analytics();
+      }
 
 	// get elements
 	const email    = document.getElementById('email');
 	const password = document.getElementById('password');
 	const login    = document.getElementById('login');
-	const signup   = document.getElementById('signup');
+	// const signup   = document.getElementById('signup');
 	const logout   = document.getElementById('logout');
 
-	// login
+	// const user = firebase.auth().currentUser;
+	// console.log('user is')
+	// console.log(user)
+	// console.log('location')
+	// console.log(window.location.pathname)
+	// if (user) {
+	// 	console.log('user is logged in')
+	// } else {
+	// 	console.log('no user logged in, redirecting to login page')
+	// 	if (window.location.pathname != '/login.html') {
+	// 	window.location.href = '/login.html'
+	// }
+		
+		
+	// 	return;
+	// }
+	// return;
+	
+
+	// // login
+	// function login(e) {
+	// 	console.log('in log in function')
+	// 	const auth  = firebase.auth();		
+	// 	const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+	// 	promise.catch(e => {
+	// 		console.log(e.message)
+	// 		target.innerHTML = `
+	// 			<div class="alert alert-warning mt-3" role="alert">
+	// 			Failed to log in. Account does not exist or email and password is incorrect. Please sign up for an account or double check your email and password is correct
+	// 		</div>`
+	// 	});
+	// }
+	if(login) {
+		
+	}
 	login.addEventListener('click', e => {
 		const auth  = firebase.auth();		
 		const promise = auth.signInWithEmailAndPassword(email.value, password.value);
 		promise.catch(e => {
 			console.log(e.message)
-			target.innerHTML = `
-				<div class="alert alert-warning mt-3" role="alert">
-				Failed to log in. Account does not exist or email and password is incorrect. Please sign up for an account or double check your email and password is correct
-			</div>`
+			// target.innerHTML = `
+			// 	<div class="alert alert-warning mt-3" role="alert">
+			// 	Failed to log in. Account does not exist or email and password is incorrect. Please sign up for an account or double check your email and password is correct
+			// </div>`
 		});
 	});
 
 	// signup
-	signup.addEventListener('click', e => {
-		// TODO: check for real email
-		const auth  = firebase.auth();
-		const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
-		promise.catch(e => {
-			console.log(e.message)
-			target.innerHTML = `
-				<div class="alert alert-danger mt-3" role="alert">
-				There was an error creating the account: ${e.message}. Please try again
-			</div>`
-		});
-	});
+	// signup.addEventListener('click', e => {
+	// 	// TODO: check for real email
+	// 	const auth  = firebase.auth();
+	// 	const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
+	// 	promise.catch(e => {
+	// 		console.log(e.message)
+	// 		target.innerHTML = `
+	// 			<div class="alert alert-danger mt-3" role="alert">
+	// 			There was an error creating the account: ${e.message}. Please try again
+	// 		</div>`
+	// 	});
+	// });
 
-    // logout
-	logout.addEventListener('click', e => {
+	// logout
+	if(logout) {
+		logout.addEventListener('click', e => {
+		console.log('clicked on log out!')
 		firebase.auth().signOut();
 		target.innerHTML = `
 			<div class="alert alert-success mt-3" role="alert">
 			Successfully logged out!
 		  </div>`
 	});
+	}
+	
 
 
     // login state
 	firebase.auth().onAuthStateChanged(firebaseUser => {
+		console.log('in on auth state changed')
 		if(firebaseUser){
+			console.log('firebase user is: ')
 			console.log(firebaseUser);
 			const userId = firebaseUser.uid;
 			firebase.database().ref('/users/' + userId).once('value', snapshot => {
@@ -79,19 +121,21 @@
 					addNewUser()
 				}
 			})
-			logout.style.display = 'inline';
-			login.style.display  = 'none';
-			signup.style.display = 'none';
-			target.innerHTML = `
-			<div class="alert alert-success mt-3" role="alert">
-			Successfully logged in! Welcome, ${firebaseUser.email}
-		  </div>`
+			// logout.style.display = 'inline';
+			// login.style.display  = 'none';
+			// signup.style.display = 'none';
+		// 	target.innerHTML = `
+		// 	<div class="alert alert-success mt-3" role="alert">
+		// 	Successfully logged in! Welcome, ${firebaseUser.email}
+		//   </div>`
+		window.location.href = '/index.html'
 		}
 		else{
 			console.log('User is not logged in');
 			logout.style.display = 'none';			
 			login.style.display  = 'inline';
 			signup.style.display = 'inline';
+			window.location.href = '/login.html'
 		}
 	});
 
